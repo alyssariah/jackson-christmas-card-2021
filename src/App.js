ff7d450e2cece7c18eceb4820fed1036927a1b14
Alyssa Jackson <alyssariah@alyssas-mbp.lan>
2022-04-02 12:30:21 -0400

    get rid of controller

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Front from "./containers/Front/Front";
 import Back from "./containers/Back/Back";
 import Timeline from "./containers/Timeline/Timeline";
 import Footer from "./containers/Footer/Footer";
-import controller from "./assets/ARKH-controller-white.glb";
 
 function App() {
   const flipCard = () => {
@@ -16,7 +15,6 @@ function App() {
     <>
       <div className="app">
         <div id="front">
-        <model-viewer id="reveal" class="visibility-hidden" alt="3d Controller" src={controller} ar ar-modes="webxr scene-viewer quick-look" shadow-intensity="1" loading="eager" camera-controls enable-pan auto-rotate></model-viewer>
           <Front flipCard={flipCard} />
         </div>
         <div id="back">
